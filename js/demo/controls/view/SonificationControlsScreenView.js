3ae6c9b54ae3c4f1a7a484f83f9d546981120164
jbphet <john.blanco@colorado.edu>
2018-07-11 11:48:45 -0600

    removed the control for basic/enhanced (now handled through nav bar)

--- js/demo/controls/view/SonificationControlsScreenView.js
+++ js/demo/controls/view/SonificationControlsScreenView.js
@@ -9,15 +9,12 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var ABSwitch = require( 'SUN/ABSwitch' );
   var Bounds2 = require( 'DOT/Bounds2' );
-  var Dimension2 = require( 'DOT/Dimension2' );
   var inherit = require( 'PHET_CORE/inherit' );
   var OneShotSoundClip = require( 'TAMBO/sound-generators/OneShotSoundClip' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var soundManager = require( 'TAMBO/soundManager' );
   var tambo = require( 'TAMBO/tambo' );
-  var Text = require( 'SCENERY/nodes/Text' );
   var TextPushButton = require( 'SUN/buttons/TextPushButton' );
 
   // audio
@@ -33,17 +30,6 @@ define( function( require ) {
       layoutBounds: new Bounds2( 0, 0, 768, 504 )
     } );
 
-    // add an AB switch that will select between 'basic' and 'enhanced' sonification
-    var abSwitch = new ABSwitch(
-      soundManager.enhancedSoundEnabledProperty,
-      false,
-      new Text( 'basic' ),
-      true,
-      new Text( 'enhanced' ),
-      { switchSize: new Dimension2( 40, 20 ), left: 100, top: 200 }
-    );
-    this.addChild( abSwitch );
-
     // create two one-shot sounds, one for basic mode and one for enhanced
     var basicModeOneShotSound = new OneShotSoundClip( marimbaSound );
     soundManager.addSoundGenerator( basicModeOneShotSound );
@@ -51,19 +37,20 @@ define( function( require ) {
     soundManager.addSoundGenerator( enhancedModeOneShotSound, { sonificationLevel: 'enhanced' } );
 
     // add a button to play a basic-mode sound
-    this.addChild( new TextPushButton( 'Play Basic-Level Sound', {
+    var playBasicSoundButton = new TextPushButton( 'Play Basic-Level Sound', {
       listener: function() { basicModeOneShotSound.play(); },
       baseColor: '#aad6cc',
-      left: abSwitch.left,
-      top: abSwitch.bottom + 10
-    } ) );
+      left: this.layoutBounds.left + 20,
+      top: this.layoutBounds.centerY
+    } );
+    this.addChild( playBasicSoundButton );
 
     // add button to play enhanced-mode sound
     this.addChild( new TextPushButton( 'Play Enhanced-Level Sound', {
       listener: function() { enhancedModeOneShotSound.play(); },
       baseColor: '#DBB1CD',
-      left: abSwitch.left,
-      top: abSwitch.bottom + 40
+      left: playBasicSoundButton.left,
+      top: playBasicSoundButton.bottom + 10
     } ) );
   }
 
