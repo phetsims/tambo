f01b1ad4d4608618db0584f02c2b4851dbb05269
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:45 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/SoundUtil.js
+++ js/SoundUtil.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * a singleton object with functions for analyzing and manipulated sound data
