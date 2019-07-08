5213562f160ae2f8f51531fd3f49bcefd9c860c7
Saurabh Totey <SaurabhTotey@gmail.com>
2019-07-08 09:19:27 -0600

    Migrated BallNode to ES6
    See #42

--- js/demo/sim-like-components/view/BallNode.js
+++ js/demo/sim-like-components/view/BallNode.js
@@ -3,11 +3,10 @@
 /**
  * a Scenery node that represents a ball in the view
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   const Circle = require( 'SCENERY/nodes/Circle' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const SoundClip = require( 'TAMBO/sound-generators/SoundClip' );
   const soundManager = require( 'TAMBO/soundManager' );
   const tambo = require( 'TAMBO/tambo' );
@@ -16,66 +15,66 @@ define( function( require ) {
   const ceilingFloorContactSound = require( 'sound!TAMBO/ceiling-floor-contact.mp3' );
   const wallContactSound = require( 'sound!TAMBO/wall-contact.mp3' );
 
-  /**
-   * @param {Ball} ball - model of a ball
-   * @param {ModelViewTransform2} modelViewTransform
-   * @constructor
-   */
-  function BallNode( ball, modelViewTransform ) {
+  class BallNode extends Circle {
 
-    const self = this;
-    Circle.call( this );
+    /**
+     * @param {Ball} ball - model of a ball
+     * @param {ModelViewTransform2} modelViewTransform
+     * @constructor
+     */
+    constructor( ball, modelViewTransform ) {
 
-    // create a circle node to represent the ball
-    const radius = modelViewTransform.modelToViewDeltaX( ball.radius );
-    Circle.call( this, radius, { fill: ball.color, stroke: 'gray' } );
+      // create a circle node to represent the ball
+      const radius = modelViewTransform.modelToViewDeltaX( ball.radius );
+      super( radius, { fill: ball.color, stroke: 'gray' } );
 
-    // move this node as the model position changes
-    ball.positionProperty.link( position => {
-      this.center = modelViewTransform.modelToViewPosition( position );
-    } );
+      // move this node as the model position changes
+      ball.positionProperty.link( position => {
+        this.center = modelViewTransform.modelToViewPosition( position );
+      } );
 
-    // add sounds
+      // add sounds
 
-    // @public (read-only) {SoundClip} - make these available so that the output level can be adjusted
-    this.wallContactSoundClip = new SoundClip( wallContactSound, {
-      initialOutputLevel: 0.3
-    } );
-    this.ceilingFloorContactSoundClip = new SoundClip( ceilingFloorContactSound, {
-      initialOutputLevel: 0.3
-    } );
+      // @public (read-only) {SoundClip} - make these available so that the output level can be adjusted
+      this.wallContactSoundClip = new SoundClip( wallContactSound, { initialOutputLevel: 0.3 } );
+      this.ceilingFloorContactSoundClip = new SoundClip( ceilingFloorContactSound, { initialOutputLevel: 0.3 } );
 
-    // add the sound generators
-    soundManager.addSoundGenerator( this.wallContactSoundClip );
-    soundManager.addSoundGenerator( this.ceilingFloorContactSoundClip );
+      // add the sound generators
+      soundManager.addSoundGenerator( this.wallContactSoundClip );
+      soundManager.addSoundGenerator( this.ceilingFloorContactSoundClip );
 
-    // play bounces when the ball bounces
-    const bounceListener = function( bounceSurface ) {
-      if ( bounceSurface === 'left-wall' || bounceSurface === 'right-wall' ) {
-        self.wallContactSoundClip.play();
-      }
-      else if ( bounceSurface === 'floor' || bounceSurface === 'ceiling' ) {
-        self.ceilingFloorContactSoundClip.play();
-      }
-    };
-    ball.bounceEmitter.addListener( bounceListener );
+      // play bounces when the ball bounces
+      const bounceListener = bounceSurface => {
+        if ( bounceSurface === 'left-wall' || bounceSurface === 'right-wall' ) {
+          this.wallContactSoundClip.play();
+        }
+        else if ( bounceSurface === 'floor' || bounceSurface === 'ceiling' ) {
+          this.ceilingFloorContactSoundClip.play();
+        }
+      };
+      ball.bounceEmitter.addListener( bounceListener );
 
-    this.disposeBallNode = function() {
-      ball.bounceEmitter.removeListener( bounceListener );
-      self.wallContactSoundClip.stop();
-      self.ceilingFloorContactSoundClip.stop();
-      soundManager.removeSoundGenerator( this.wallContactSoundClip );
-      soundManager.removeSoundGenerator( this.ceilingFloorContactSoundClip );
-    };
+      this.disposeBallNode = () => {
+        ball.bounceEmitter.removeListener( bounceListener );
+        this.wallContactSoundClip.stop();
+        this.ceilingFloorContactSoundClip.stop();
+        soundManager.removeSoundGenerator( this.wallContactSoundClip );
+        soundManager.removeSoundGenerator( this.ceilingFloorContactSoundClip );
+      };
+    }
+
+    /**
+     * @public
+     * @override
+     */
+    dispose() {
+      this.disposeBallNode();
+      super.dispose();
+    }
+
   }
 
   tambo.register( 'BallNode', BallNode );
 
-  return inherit( Circle, BallNode, {
-
-    dispose: function() {
-      this.disposeBallNode();
-      Circle.prototype.dispose.call( this );
-    }
-  } );
+  return BallNode;
 } );
