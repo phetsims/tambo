2d56b6e6521a367af6b5900ffb3bb7907ebe3db8
jbphet <john.blanco@colorado.edu>
2019-10-16 14:04:37 -0600

    added require-able version of sound

---
+++ js/shared-sound-players/checkboxUncheckedSound.js
@@ -0,0 +1,24 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * shared sound generator for un-checking a checkbox
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const SharedSoundClip = require( 'TAMBO/sound-generators/SharedSoundClip' );
+  const tambo = require( 'TAMBO/tambo' );
+
+  // sounds
+  const checkboxUncheckedSoundInfo = require( 'sound!TAMBO/check-box-unchecked.mp3' );
+
+  // create the shared sound instance
+  const checkboxUncheckedSound = new SharedSoundClip( checkboxUncheckedSoundInfo, { initialOutputLevel: 0.7 } );
+
+  tambo.register( 'checkboxUncheckedSound', checkboxUncheckedSound );
+
+  return checkboxUncheckedSound;
+} );
