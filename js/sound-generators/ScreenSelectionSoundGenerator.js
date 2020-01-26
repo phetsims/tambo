5691ef0d7ff44f246f7ad364b094e1adb3050ea3
phet-dev <phetadmin@gmail.com>
2020-01-26 03:34:09 -0700

    update copyright dates from daily grunt work

--- js/sound-generators/ScreenSelectionSoundGenerator.js
+++ js/sound-generators/ScreenSelectionSoundGenerator.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Generates sounds for when the user switches between screens and screen icons
