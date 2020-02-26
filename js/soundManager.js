dc25b0c23c734e43ff19c4abf2f82845d2d71732
zepumph <michael.kauzmann@colorado.edu>
2020-02-26 14:05:15 -0900

    remove phet.inWrapper logic, https://github.com/phetsims/tambo/issues/96

--- js/soundManager.js
+++ js/soundManager.js
@@ -49,7 +49,7 @@ define( require => {
       this.enabledProperty = new BooleanProperty( phet.chipper.queryParameters.sound === 'enabled', {
 
         // so that tambo can be used in a wrapper it is necessary to short circuit tandem creation in some cases
-        tandem: phet.inWrapper ? Tandem.OPT_OUT : SOUND_MANAGER_TANDEM.createTandem( 'enabledProperty' ),
+        tandem: SOUND_MANAGER_TANDEM.createTandem( 'enabledProperty' ),
         phetioFeatured: true,
         phetioDocumentation: 'If the sim contains sound, then this Property will toggle whether sound is enabled or ' +
                              'disabled.'
