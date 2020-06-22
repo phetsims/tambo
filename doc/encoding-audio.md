57dc01ee8e2ccf18287244362e90bc96b052e191
jbphet <john.blanco@colorado.edu>
2020-06-22 16:44:39 -0600

    doc update

--- doc/encoding-audio.md
+++ doc/encoding-audio.md
@@ -2,12 +2,14 @@ Encoding Audio
 ==============
 
 A fair amount of testing was done to determine the optimum tradeoff between audio file size and quality.  The
-recommendations at the time of this writing are:
+current recommendations are:
 
-+ Always keep the full .wav file around so that we can re-encode if needed
-+ Audio files used in the sims should be in .mp3 format, encoded at 48 kbps unless a higher resolution is needed
-+ Unless there is a specific sound design reason for stereo files, audio file should be mono, not stereo
++ Always keep the full .wav file around in the `assets` direction so that we can re-encode if needed.
++ Audio files used in the sim code should be in .mp3 format in almost all cases, encoded at 48 kbps unless a higher
+resolution is needed.  The one exception to this is if a loop is needed with continuous sound playing.  Due to the short
+silence that generally exists at the beginning of an .mp3 file, and .wav file is sometimes required. 
++ Mono sounds should be used unless there is a specific sound-design need for stereo sounds.
 
-See https://github.com/phetsims/tambo/issues/28 for more information about the history and though process behing these
-recommendations.
+See https://github.com/phetsims/tambo/issues/28 for more information about the history of and thought process behind
+these recommendations.
  
--- js/phetAudioContext.js
+++ js/phetAudioContext.js
@@ -1,132 +1,28 @@
 // Copyright 2018-2020, University of Colorado Boulder
 
 /**
- * phetAudioContext is a singleton that is either a standard Web Audio AudioContext instance or a stubbed version.  The
- * stubbed version is used on platforms where Web Audio is not supported.
+ * phetAudioContext is a singleton instance of a Web Audio context that is used for all Web Audio interaction
+ *
  *
  * @author John Blanco
  */
 
 import tambo from './tambo.js';
 
-// constants
-const FORCE_STUBBED_AUDIO_CONTEXT = phet.chipper.queryParameters.forceStubbedAudioContext; // used for testing, see below
-
-// helper function for logging warnings
-function logUnimplementedWarning() {
-  assert && console.warn( 'warning: method called on stubbed audio context, no sound will be produced' );
-}
-
-// silent stub function, used in the stubbed audio context
-function silentStub() {}
-
-// Define stubbed objects and ultimately an entire stubbed AudioContext that can be used in cases where browsers don't
-// support Web Audio.  This was created manually by identifying what portions of the Web Audio API were being used in
-// tambo and adding those methods and properties. Please keep methods and properties are in alphabetical order for ease
-// of maintenance.  These objects will likely need to be updated periodically as tambo and our usage of Web Audio
-// evolves.  See https://github.com/phetsims/tambo/issues/10.
-
-const STUBBED_AUDIO_PARAM = {
-  cancelScheduledValues: silentStub,
-  linearRampToValueAtTime: silentStub,
-  setTargetAtTime: silentStub,
-  setValueAtTime: silentStub
-};
-
-const STUBBED_OSCILLATOR_NODE = {
-  connect: silentStub,
-  frequency: STUBBED_AUDIO_PARAM,
-  start: silentStub
-};
-
-const STUBBED_GAIN_NODE = {
-  connect: silentStub,
-  disconnect: silentStub,
-  gain: STUBBED_AUDIO_PARAM
-};
-
-const STUBBED_DYNAMICS_COMPRESSOR_NODE = {
-  attack: {
-    setValueAtTime: silentStub
-  },
-  connect: silentStub,
-  knee: {
-    setValueAtTime: silentStub
-  },
-  ratio: {
-    setValueAtTime: silentStub
-  },
-  release: {
-    setValueAtTime: silentStub
-  },
-  threshold: {
-    setValueAtTime: silentStub
-  }
-};
-
-const STUBBED_AUDIO_BUFFER_SOURCE_NODE = {
-  connect: silentStub,
-  disconnect: silentStub,
-  playbackRate: STUBBED_AUDIO_PARAM,
-  start: silentStub,
-  stop: silentStub
-};
-
-const STUBBED_CONVOLVER_NODE = {
-  connect: silentStub
-};
-
-const STUBBED_BIQUAD_FILTER_NODE = {
-  connect: silentStub,
-  frequency: STUBBED_AUDIO_PARAM,
-  Q: STUBBED_AUDIO_PARAM
-};
-
-const STUBBED_AUDIO_BUFFER = {
-  getChannelData: function() {
-    return {};
-  }
-};
-
-// Define a stubbed audio context (basically a subset of the API for AudioContext) that can be used in cases where
-// browsers don't support Web Audio.  This was created manually by identifying what portions of the Web Audio API were
-// being used in tambo and adding those methods and properties. This may need to be updated periodically as tambo and
-// our usage of Web Audio evolves.  See https://github.com/phetsims/tambo/issues/10.
-const STUBBED_AUDIO_CONTEXT = {
-
-  // methods and properties are in alphabetical order, please maintain this for ease of maintenance
-  createBiquadFilter: () => STUBBED_BIQUAD_FILTER_NODE,
-  createBuffer: () => STUBBED_AUDIO_BUFFER,
-  createBufferSource: () => STUBBED_AUDIO_BUFFER_SOURCE_NODE,
-  createConvolver: () => STUBBED_CONVOLVER_NODE,
-  createDynamicsCompressor: () => STUBBED_DYNAMICS_COMPRESSOR_NODE,
-  createGain: () => STUBBED_GAIN_NODE,
-  createOscillator: () => STUBBED_OSCILLATOR_NODE,
-  currentTime: 0,
-  decodeAudioData: ( arrayBuffer, successCallback, errorCallback ) => {
-    successCallback( {} );
-  },
-  destination: null,
-  resume: logUnimplementedWarning,
-  state: 'running',
-
-  // this is a flag that is specific to the stubbed audio context, allowing it to be identified
-  isStubbed: true
-};
-
 // create a Web Audio context
 let phetAudioContext = null;
-if ( window.AudioContext && !FORCE_STUBBED_AUDIO_CONTEXT ) {
+if ( window.AudioContext ) {
   phetAudioContext = new window.AudioContext();
 }
-else if ( window.webkitAudioContext && !FORCE_STUBBED_AUDIO_CONTEXT ) {
+else if ( window.webkitAudioContext ) {
   phetAudioContext = new window.webkitAudioContext();
 }
 else {
 
-  // the browser doesn't support creating an audio context, so use a stubbed out version
-  phetAudioContext = STUBBED_AUDIO_CONTEXT;
-  console.warn( 'warning: using stubbed audio context' );
+  // The browser doesn't support creating an audio context, create an empty object.  Failures will occur the first time
+  // any code tries to do anything with the audio context.
+  phetAudioContext = {};
+  console.error( 'error: this browser does not support Web Audio' );
 }
 
 // register for phet-io
