debbf9f0ac0050410fc6945a94b824a92a0d963c
jbphet <john.blanco@colorado.edu>
2020-08-05 12:13:58 -0600

    created shared sound player for reaching a boundary, integrated into Faraday's Law, see https://github.com/phetsims/faradays-law/issues/167

---
+++ js/shared-sound-players/boundaryReachedSoundPlayer.js
@@ -0,0 +1,20 @@
+// Copyright 2019-2020, University of Colorado Boulder
+
+/**
+ * shared sound generator for when something encounters a boundary
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+
+import checkboxCheckedSound from '../../sounds/boundary-reached_mp3.js';
+import SharedSoundClip from '../sound-generators/SharedSoundClip.js';
+import tambo from '../tambo.js';
+
+// create the shared sound instance
+const boundaryReachedSoundPlayer = new SharedSoundClip( checkboxCheckedSound, {
+  soundClipOptions: { initialOutputLevel: 0.8 },
+  soundManagerOptions: { categoryName: 'user-interface' }
+} );
+
+tambo.register( 'boundaryReachedSoundPlayer', boundaryReachedSoundPlayer );
+export default boundaryReachedSoundPlayer;
