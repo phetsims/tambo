39a2fec7f706284ec421c02d34c27106068af72b
jbphet <john.blanco@colorado.edu>
2021-03-30 17:32:19 -0600

    improved documentation, see https://github.com/phetsims/tambo/issues/133

--- js/PeakDetectorAudioNode.js
+++ js/PeakDetectorAudioNode.js
@@ -2,8 +2,9 @@
 
 /**
  * PeakDetectorAudioNode is a Web Audio node that can be used to detect peak audio output values in an audio signal
- * chain.  This is the portion that runs in the main JavaScript thread.  There is a counterpart portion that runs in
- * the Web Audio rendering thread.
+ * chain.  The detected peak audio values are output to the console.  This file contains the portion that runs in the
+ * main JavaScript thread, which appears to be referred to as the "AudioWorklet Node".  There is a counterpart portion
+ * that runs in the Web Audio rendering thread that is referred to as the "AudioWorklet Processor".
  *
  * @author John Blanco (PhET Interactive Simulations)
  */
@@ -16,19 +17,20 @@ class PeakDetectorAudioNode extends AudioWorkletNode {
 
   /**
    * @param {Object} [options]
-   *
    */
   constructor( options ) {
 
     options = merge( {
 
-      // {boolean} - if true, zero values will be output, otherwise they will be ignored
+      // {boolean} - If true, zero values will be output, otherwise no output will occur if the peak value detected
+      //             for a given time interval is zero.
       logZeroValues: false
 
     }, options );
 
     super( phetAudioContext, 'peak-detector' );
 
+    // Listen for messages from the audio worklet processor and log peak values to the console.
     this.port.onmessage = event => {
       if ( event.data.peak !== undefined ) {
         const peak = event.data.peak;
--- js/peak-detector.js
+++ js/peak-detector.js
@@ -1,8 +1,8 @@
 // Copyright 2021, University of Colorado Boulder
 
 /**
- * This file defines the portion of the peak detector that runs on the audio rendering thread in the
- * AudioWorkletGlobalScope.  It must be loaded using audioWorklet.addModule in the main JavaScript thread.
+ * This file defines an AudioWorklet processor for a peak detector.  This code is intended to run on the audio rendering
+ * thread in the AudioWorkletGlobalScope.  It must be loaded using audioWorklet.addModule in the main JavaScript thread.
  *
  * @author John Blanco (PhET Interactive Simulations)
  */
@@ -18,7 +18,7 @@ class PeakDetectorProcessor extends AudioWorkletProcessor {
   }
 
   /**
-   * Process the audio information - see the Web Audio documentation for details about this method.
+   * Process the audio information - see the Web Audio documentation for details about the parameters for this method.
    * @param inputs
    * @param outputs
    * @param parameters
@@ -29,13 +29,14 @@ class PeakDetectorProcessor extends AudioWorkletProcessor {
     const now = Date.now();
     const input = inputs[ 0 ];
 
-    // Scan through the input data and see if the peak value has been exceeded.  This only looks at one channel of the
-    // first input, since that's all that has been needed so far.
+    // Scan through the input data and see if the peak value has been exceeded and, if so, set a new value.  This only
+    // looks at one channel of the first input, since that's all that has been needed so far.
     const channelData = input[ 0 ];
     for ( let i = 0; i < channelData.length; i++ ) {
       this.peak = Math.max( Math.abs( channelData[ i ] ), this.peak );
     }
 
+    // If the update period has been exceeded, send a message to the main thread with the current peak and reset it.
     if ( now - this.lastUpdateTime >= UPDATE_PERIOD ) {
       this.port.postMessage( { peak: this.peak } );
 
