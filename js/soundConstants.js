a1dfc4b49e169afceaad0b7496ce9528dbc13f62
phet-dev <phetadmin@gmail.com>
2021-06-30 03:49:32 -0600

    update copyright dates from daily grunt work

--- js/soundConstants.js
+++ js/soundConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * a singleton instance that contains constants shared amongst the sound generation code modules
