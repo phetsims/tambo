059018315915c5e4a0de93bb28e114b00057c3a3
phet-dev <phetadmin@gmail.com>
2021-11-26 03:22:36 -0700

    update copyright dates from daily grunt work

--- js/demo/SoundOptionsDialogContent.js
+++ js/demo/SoundOptionsDialogContent.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2021, University of Colorado Boulder
 
 import NumberProperty from '../../../axon/js/NumberProperty.js';
 import PhetFont from '../../../scenery-phet/js/PhetFont.js';
