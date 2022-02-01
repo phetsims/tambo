7b539447db9da61f96d4cdc2ecd7e142555b3e6c
phet-dev <phetadmin@gmail.com>
2022-02-01 04:00:36 -0700

    update copyright dates from daily grunt work

--- js/demo/testing/view/TestingScreenView.js
+++ js/demo/testing/view/TestingScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * view for a screen that allows testing and demonstration of various sound components and behaviors
