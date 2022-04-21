69cc0da5f6adcad34f74c7e63e5f4ce774022192
zepumph <michael.kauzmann@colorado.edu>
2022-04-21 08:31:49 -0600

    Add step() to base class + overrides, https://github.com/phetsims/joist/issues/795

--- js/demo/testing/view/TestingScreenView.ts
+++ js/demo/testing/view/TestingScreenView.ts
@@ -125,7 +125,7 @@ class TestingScreenView extends DemosScreenView {
     this.addChild( resetAllButton );
   }
 
-  public step( dt: number ) {
+  public override step( dt: number ) {
     this.stepEmitter.emit( dt );
   }
 }
