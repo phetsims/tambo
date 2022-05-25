382620e4ce6996491b6b24fead907762fd081d51
jbphet <john.blanco@colorado.edu>
2022-05-25 14:40:57 -0600

    added sounds and shared sound players for first cut at AB switch sounds, see https://github.com/phetsims/joist/issues/803

---
+++ js/shared-sound-players/switchToASoundPlayer.ts
@@ -0,0 +1,20 @@
+// Copyright 2020-2022, University of Colorado Boulder
+
+/**
+ * Shared sound generator, primarily for the AB switch, but potentially useful elsewhere.  Uses the singleton pattern.
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+
+import switchToA_mp3 from '../../sounds/switchToA_mp3.js';
+import SoundClipPlayer from '../sound-generators/SoundClipPlayer.js';
+import tambo from '../tambo.js';
+
+// create the shared sound instance
+const switchToASoundPlayer = new SoundClipPlayer( switchToA_mp3, {
+  soundClipOptions: { initialOutputLevel: 0.5 },
+  soundManagerOptions: { categoryName: 'user-interface' }
+} );
+
+tambo.register( 'switchToASoundPlayer', switchToASoundPlayer );
+export default switchToASoundPlayer;
---
+++ js/shared-sound-players/switchToBSoundPlayer.ts
@@ -0,0 +1,20 @@
+// Copyright 2020-2022, University of Colorado Boulder
+
+/**
+ * Shared sound generator, primarily for the AB switch, but potentially useful elsewhere.  Uses the singleton pattern.
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+
+import switchToB_mp3 from '../../sounds/switchToB_mp3.js';
+import SoundClipPlayer from '../sound-generators/SoundClipPlayer.js';
+import tambo from '../tambo.js';
+
+// create the shared sound instance
+const switchToBSoundPlayer = new SoundClipPlayer( switchToB_mp3, {
+  soundClipOptions: { initialOutputLevel: 0.5 },
+  soundManagerOptions: { categoryName: 'user-interface' }
+} );
+
+tambo.register( 'switchToBSoundPlayer', switchToBSoundPlayer );
+export default switchToBSoundPlayer;
