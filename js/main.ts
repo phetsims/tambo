5740cf3748e98643208cbf2a03ed0e1d052fcad4
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:10 -0600

    Updating phet-lib with main common code references.

---
+++ js/main.ts
@@ -0,0 +1,58 @@
+// Copyright 2022, University of Colorado Boulder
+
+import tambo from './tambo.js';
+
+import './shared-sound-players/accordionBoxClosedSoundPlayer.js';
+import './shared-sound-players/accordionBoxOpenedSoundPlayer.js';
+import './shared-sound-players/boundaryReachedSoundPlayer.js';
+import './shared-sound-players/checkboxCheckedSoundPlayer.js';
+import './shared-sound-players/checkboxUncheckedSoundPlayer.js';
+import './shared-sound-players/generalBoundaryBoopSoundPlayer.js';
+import './shared-sound-players/generalCloseSoundPlayer.js';
+import './shared-sound-players/generalOpenSoundPlayer.js';
+import './shared-sound-players/generalSoftClickSoundPlayer.js';
+import './shared-sound-players/grabSoundPlayer.js';
+import './shared-sound-players/nullSoundPlayer.js';
+import './shared-sound-players/pauseSoundPlayer.js';
+import './shared-sound-players/playSoundPlayer.js';
+import './shared-sound-players/pushButtonSoundPlayer.js';
+import './shared-sound-players/releaseSoundPlayer.js';
+import './shared-sound-players/resetAllSoundPlayer.js';
+import './shared-sound-players/softClickSoundPlayer.js';
+import './shared-sound-players/stepBackwardSoundPlayer.js';
+import './shared-sound-players/stepForwardSoundPlayer.js';
+import './shared-sound-players/switchToLeftSoundPlayer.js';
+import './shared-sound-players/switchToRightSoundPlayer.js';
+import './shared-sound-players/toggleOffSoundPlayer.js';
+import './shared-sound-players/toggleOnSoundPlayer.js';
+
+import './sound-generators/CompositeSoundClip.js';
+import './sound-generators/ContinuousPropertySoundGenerator.js';
+import './sound-generators/DiscreteSoundGenerator.js';
+import './sound-generators/MultiClip.js';
+import './sound-generators/NoiseGenerator.js';
+import './sound-generators/OscillatorSoundGenerator.js';
+import './sound-generators/PitchedPopGenerator.js';
+import './sound-generators/PropertyMultiClip.js';
+import './sound-generators/SoundClip.js';
+import './sound-generators/SoundClipChord.js';
+import './sound-generators/SoundClipPlayer.js';
+import './sound-generators/SoundGenerator.js';
+import './sound-generators/ValueChangeSoundPlayer.js';
+
+import './AmplitudeModulator.js';
+import './BinMapper.js';
+import './PeakDetectorAudioNode.js';
+import './SoundLevelEnum.js';
+import './SoundUtils.js';
+import './TSoundPlayer.js';
+import './TamboStrings.js';
+import './WrappedAudioBuffer.js';
+import './audioContextStateChangeMonitor.js';
+import './base64SoundToByteArray.js';
+import './multiSelectionSoundPlayerFactory.js';
+import './phetAudioContext.js';
+import './soundConstants.js';
+import './soundManager.js';
+
+export default tambo;
