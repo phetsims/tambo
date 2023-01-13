9eea7d27a6a13b8a3f9ac71f6c00fdc71c4eeeff
phet-dev <phetadmin@gmail.com>
2023-01-13 04:35:46 -0700

    update copyright dates from daily grunt work

--- js/demo/ui-components/view/UIComponentsScreenView.ts
+++ js/demo/ui-components/view/UIComponentsScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * UIComponentsScreenView is a view for a screen that demonstrates views and sounds for common User Interface
