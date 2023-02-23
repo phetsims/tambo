b2005d822973ab90f8c515788f4828727576f433
phet-dev <phetadmin@gmail.com>
2023-02-23 05:25:22 -0700

    update copyright dates from daily grunt work

--- js/sound-generators/ContinuousPropertySoundGenerator.ts
+++ js/sound-generators/ContinuousPropertySoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * ContinuousPropertySoundGenerator is a sound generator that alters the playback rate of a sound clip based on the
