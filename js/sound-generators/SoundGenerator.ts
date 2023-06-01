0da95177bc813fa853d9cb54b905ef5ad31f1208
phet-dev <phetadmin@gmail.com>
2023-06-01 03:34:28 -0600

    update copyright dates from daily grunt work

--- js/sound-generators/SoundGenerator.ts
+++ js/sound-generators/SoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * SoundGenerator is an abstract base class for Web-Audio-based sound-producing elements that work in conjunction with
