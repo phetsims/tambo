050a0a7a25947001b8d2d958291729c4e095ffa0
pixelzoom <cmalley@pixelzoom.com>
2024-01-05 09:32:06 -0700

    fixes for author-annotation lint rule, https://github.com/phetsims/chipper/issues/1414

--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,9 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
 import tambo from './tambo.js';
 
 import './shared-sound-players/accordionBoxClosedSoundPlayer.js';
