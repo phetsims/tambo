e35bc266b15f52b0295c3e09176e388027ef7af0
phet-dev <phetadmin@gmail.com>
2024-09-24 03:59:01 -0600

    update copyright dates from daily grunt work

--- js/AmplitudeModulator.ts
+++ js/AmplitudeModulator.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 
 /**
  * AmplitudeModulator instances can be used to create low-frequency amplitude oscillation effects on a loop, oscillator,
--- js/BinMapper.ts
+++ js/BinMapper.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * BinMapper is an object that maps a continuous value to one of a finite number of "bins".
