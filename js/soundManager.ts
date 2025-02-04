5026bddfce0800afe089d25ad2202aec2c9b1c31
phet-dev <phetadmin@gmail.com>
2025-02-04 04:42:59 -0700

    update copyright dates from daily grunt work

--- js/soundManager.ts
+++ js/soundManager.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2024, University of Colorado Boulder
+// Copyright 2018-2025, University of Colorado Boulder
 
 /**
  * A singleton object that registers sound generators, connects them to the audio output, and provides a number of
